a44a808971cfa613843ddff1146525524789f576
frankiederosier <frankiederosier@Frankies-MBP.lan>
2024-03-24 18:15:24 -0400

    utils

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
+const routes = require ('./controllers');
 const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
@@ -11,19 +11,21 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({helpers});
 
 const sess = {
-  secret: 'Super secret secret',
-  // session will expire and logout user after 20 minutes
-  cookie: {
-    expires: 20 * 60 * 1000
-  },
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
+    secret: 'super secret secret',
+    cookie: {
+        maxAge: 300000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+    },
+    resave: false,
+saveUninitialized:true,
+store: new SequelizeStore({
     db: sequelize
-  })
+})
 };
 
 app.use(session(sess));
@@ -32,11 +34,11 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+sequelize.sync({force: false}).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
